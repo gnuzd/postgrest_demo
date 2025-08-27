d0ee2a483ddc8e69a3b0f0bfe0f5c24113d75217
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-08-27 19:19:45 +0700

    feat: init chatapp

---
+++ chatapp/src/routes/register/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import {
+		Card,
+		CardContent,
+		CardDescription,
+		CardHeader,
+		CardTitle
+	} from '$lib/components/ui/card';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+</script>
+
+<div class="flex items-center justify-center min-h-screen py-12">
+	<Card class="mx-auto w-full max-w-md">
+		<CardHeader>
+			<CardTitle class="text-xl">Sign Up</CardTitle>
+			<CardDescription>Enter your information to create an account</CardDescription>
+		</CardHeader>
+		<CardContent>
+			<div class="grid gap-4">
+				<div class="grid gap-2">
+					<Label for="email">Email</Label>
+					<Input id="email" type="email" placeholder="m@example.com" required />
+				</div>
+				<div class="grid gap-2">
+					<Label for="password">Password</Label>
+					<Input id="password" type="password" required />
+				</div>
+				<Button type="submit" class="w-full">Create an account</Button>
+			</div>
+			<div class="mt-4 text-center text-sm">
+				Already have an account?
+				<a href="/login" class="underline"> Sign in </a>
+			</div>
+		</CardContent>
+	</Card>
+</div>
